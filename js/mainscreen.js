785498d6e2bfd971112ddea9c99149e1c8a06432
Pavan <dayalpavan@gmail.com>
2017-02-11 17:06:52 -0500

    push from sublime text

--- js/mainscreen.js
+++ js/mainscreen.js
@@ -1,10 +1,7 @@
 var setOrientation = function(i) {
-	switch (i) {
-		case 1:
-			alert("1");
-			break;
-		case 2:
-			alert("2");
-			break;
-	}
+	var clickedOrientation = document.getElementById("orientation" + i);
+	var otherOrientation = document.getElementById("orientation" + (3-i));
+
+	//clickedOrientation.style.opacity = "1.0";
+	//otherOrientation.style.opacity = "0.5";
 }
