5fbefcebad6390f40aead56c7f5af6aec880689a
Pavan <dayalpavan@gmail.com>
2017-02-11 19:27:18 -0500

    push from sublime text

--- js/mainscreen.js
+++ js/mainscreen.js
@@ -1,4 +1,9 @@
+var orientation = 1;
+var page = document.getElementById("page");
+
 var setOrientation = function(i) {
+	orientation = i;
+
 	var clickedOrientation = document.getElementById("orientation" + i);
 	var otherOrientation = document.getElementById("orientation" + (3-i));
 
@@ -6,6 +11,6 @@ var setOrientation = function(i) {
 	otherOrientation.style.opacity = "0.70";
 }
 
-var scoutMatch = function () {
-	document.write("lololol");
+var scoutMatch = function() {
+	page.innerHTML = "<p>asdf</p>";
 }
