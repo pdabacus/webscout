6eb35821ddf738f575e2977241bb6ed585c85846
Pavan <dayalpavan@gmail.com>
2017-02-12 16:33:02 -0500

    push from sublime text

--- app/2017/hub/index.php
+++ app/2017/hub/index.php
@@ -8,13 +8,26 @@
     <script type="text/javascript" src="js/hub.js"></script>
     <link rel="stylesheet" type="text/css" href="../../../css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../../../css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body id="body">
     <div class="container-fluid" style="text-align: center;">
+      <!-- left spacer -->
       <div class="col-sm-1 col-md-2"></div>
+      <!-- main page -->
       <div id="page" class="fadein col-xs-12 col-sm-10 col-md-8">
         <h1>Steamworks 2017 Scouting Hub</h1>
+        <hr/>
+        <input id="user" class="textinput" type="text" placeholder="Username"/>
+        <br/>
+        <input id="pass" class="textinput" type="password" placeholder="Password"/>
+        <hr/>
+        <!-- footer -->
+        <div id="footer"">
+          <div>Copyright &copy; <?php echo date("Y"); ?> Swift Creek Robotics</div>
+        </div>
       </div>
+      <!-- right spacer -->
       <div class="col-sm-1 col-md-2"></div>
     </div>
   </body>
