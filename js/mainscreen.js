17d673ef7d151dc1532ef8bca15c3960338038a8
Pavan <dayalpavan@gmail.com>
2017-02-11 19:38:05 -0500

    push from sublime text

--- js/mainscreen.js
+++ js/mainscreen.js
@@ -13,8 +13,10 @@ var setOrientation = function(i) {
 var scoutMatch = function() {
 	var body = document.getElementById("body");
 	$("#body").fadeOut(1000);
-	setTimeout(null,1000);
-	body.innerHTML = "<p>orientation" + orientation + "</p>";
-	$("#body").fadeIn(1000);
+	setTimeout(function() {
+		body.innerHTML = "<p>orientation" + orientation + "</p>";
+		$("#body").fadeIn(1000);
+	},1000);
+	
 
 }
