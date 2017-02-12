0735e07112c32d358283ecb22b4dacb32d08effc
Pavan <dayalpavan@gmail.com>
2017-02-11 20:43:20 -0500

    push from sublime text

--- js/mainscreen.js
+++ js/mainscreen.js
@@ -7,11 +7,30 @@ var setOrientation = function(i) {
 	var otherOrientation = $("#orientation" + (3-i));
 
 	clickedOrientation.css("opacity", "1.0");
-	otherOrientation.css("opacity", "0.70");
+	otherOrientation.css("opacity", "0.75");
+
+	clickedOrientation.off("mouseenter mouseleave");
+	otherOrientation.mouseenter(function() {
+		$(this).css("opacity", "0.90");
+	});
+	otherOrientation.mouseleave(function() {
+		$(this).css("opacity", "0.75");
+	});
 }
 
 var errorHTML = function(msg) {
 	return '<h4 style="color: #FF3344">ERROR: ' + msg + '</h4>';
+}
+
+var transitionBody = function(html, time) {
+		var body = $("#body");
+
+		body.fadeOut(time);
+		setTimeout(function() {
+			body.html(html);
+			body.fadeIn(time);
+		}, time);
+
 }
 
 var scoutMatch = function() {
@@ -24,11 +43,7 @@ var scoutMatch = function() {
 		html += '<div>Scout Match</div>';
 		html += '</div>';
 		$("#scoutMatch").html(html);
-	} else { 
-
-		var body = $("#body");
-		var time = 500;
-
+	} else {
 		html = '<div class="container-fluid" style="text-align: center;">';
 		html += '<div class="col-xs-1 col-sm-2 col-md-3"></div>';
 		html += '<div class="col-xs-10 col-sm-8 col-md-6" style="margin: 40px auto">';
@@ -37,12 +52,7 @@ var scoutMatch = function() {
 		html += '<div class="col-xs-1 col-sm-2 col-md-3"></div>';
 		html += '</div>';
 
-		body.fadeOut(time);
-		setTimeout(function() {
-			body.html(html);
-			body.fadeIn(time);
-		}, time);
-	
+		transitionBody(html, 500);
 	}
 
 }
