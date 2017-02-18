1a34cae59c6557dcb09d11d5565d1651023d5bb7
Pavan <dayalpavan@gmail.com>
2017-02-18 18:55:04 -0500

    push from sublime text

--- app/2017/scout/js/scout.js
+++ app/2017/scout/js/scout.js
@@ -10,4 +10,26 @@ var transitionElement = function(elem, html, time) {
 
 $(document).ready(function() {
 	$(".fadein").fadeIn(500);
+	setTimeout(loadForm, 1200);
 });
+
+var loadForm = function() {
+	$("#body").fadeOut(500);
+	setTimeout(function() {
+
+		var html = '<div class="col-xs-1 col-sm-2 col-md-3"></div>';
+		html += '<div class="col-xs-10 col-sm-8 col-md-6" style="margin: 40px auto">';
+
+		html += "<h2>Team " + main.team + " Scouting:</h2>";
+		html += "<h3>" + main.matchType + " " + main.matchNumber + " match</h3>";
+		html += "<h3>" + main.alliance + " alliance</h3>";
+		html += "<h3>robot " + main.robot + "</h3>";
+
+		html += '</div>';
+		html += '<div class="col-xs-1 col-sm-2 col-md-3"></div>';
+
+		$("#body").html(html);
+		$("#body").fadeIn(500);
+	}, 500);
+}
+
