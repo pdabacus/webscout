37f06c8a514051ccbf16096b1006590b79753e95
Pavan <dayalpavan@gmail.com>
2017-02-19 18:02:07 -0500

    push from sublime text

--- data/FileManager.py
+++ data/FileManager.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 from Record import *
-from Auth import *
+from AccountSystem import *
 
 r = Record.open(4828, "worlds.json")
+r.addMatch(Match("qual", 1, [1,2,3], [4,5,6]))
 print(r)
+print()
+
+a = AccountSystem.open("passwd")
+a.addUser(User("4828", "de3f712d1a02c5fb481a7a99b0da7fa3"))
+print(a)
